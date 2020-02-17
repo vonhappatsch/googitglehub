0489dcf792f9799f82d346550523e71ad572e039
Jéssica Happatsch <jessica.p.happatsch@accenture.com>
2020-02-17 19:40:19 -0300

    Updates

---
+++ precache-manifest.53d1dc2820a75324897cba20a7e7e2e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d1c74a15a2ddbe979ba6809c738ac8",
+    "url": "/googitglehub/index.html"
+  },
+  {
+    "revision": "ff17744435c99091328e",
+    "url": "/googitglehub/static/css/main.dd247f75.chunk.css"
+  },
+  {
+    "revision": "8c21b73d1c921e6bbc6a",
+    "url": "/googitglehub/static/js/2.5db5914b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/googitglehub/static/js/2.5db5914b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff17744435c99091328e",
+    "url": "/googitglehub/static/js/main.9a234786.chunk.js"
+  },
+  {
+    "revision": "a5f6a7d70325a22ed97a",
+    "url": "/googitglehub/static/js/runtime-main.9f929ce5.js"
+  }
+]);
